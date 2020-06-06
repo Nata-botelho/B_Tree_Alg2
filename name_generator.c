b0d12feda38d4d8d86a3b469fd95b8b37c8f90da
Natã Botelho <nbotelho789@hotmail.com>
2020-06-06 02:30:52 -0300

    gerador de nomes atualizado

--- name_generator.c
+++ name_generator.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string.h>
 
-char *getRandomName(){
+char *getRandomName(int flag){
 
     static char **sampleList;
     static int counter = 0;
@@ -13,23 +13,29 @@ char *getRandomName(){
         if(!sample_file){printf("Erro no arquivo de amostras de nomes!"); exit(0);}
 
         sampleList = (char**) malloc (sizeof(char*));
-
         char *aux = (char*) malloc (sizeof(char)*16);
-        printf("miniboi ");
-        while(fscanf(sample_file, "%s", aux)){
-            printf("%d\n", counter);
+
+        while(fscanf(sample_file, "%s\n", aux) == 1){
             sampleList[counter] = strdup(aux);
             counter++;
-            sampleList = (char**) realloc (sampleList, sizeof(char*)*counter);
+            sampleList = (char**) realloc (sampleList, sizeof(char*)*(counter+1));
         }
 
         fclose(sample_file);
     }
 
-    return sampleList[rand()%counter];
+    if(flag)
+        return sampleList[rand()%counter];
+    else{
+        for(int i = 0; i < counter; i++)
+            free(sampleList[i]);
+        free(sampleList);
+        return NULL;
+    }
 }
 
-char *getRandomLastName(){
+char *getRandomLastName(int flag){
+
     static char **sampleList;
     static int counter = 0;
 
@@ -38,20 +44,29 @@ char *getRandomLastName(){
         if(!sample_file){printf("Erro no arquivo de amostras de sobrenomes!"); exit(0);}
 
         sampleList = (char**) malloc (sizeof(char*));
-        char *aux = NULL;
-        while(fscanf(sample_file, "%s\n", aux)){
+        char *aux = (char*) malloc (sizeof(char)*16);
+
+        while(fscanf(sample_file, "%s\n", aux) == 1){
             sampleList[counter] = strdup(aux);
             counter++;
-            sampleList = (char**) realloc (sampleList, sizeof(char*)*counter);
+            sampleList = (char**) realloc (sampleList, sizeof(char*)*(counter+1));
         }
 
         fclose(sample_file);
     }
 
-    return sampleList[rand()%counter];
+    if(flag)
+        return sampleList[rand()%counter];
+    else{
+        for(int i = 0; i < counter; i++)
+            free(sampleList[i]);
+        free(sampleList);
+        return NULL;
+    }
 }
 
-char *getRandomCourse(){
+char *getRandomCourse(int flag){
+    
     static char **sampleList;
     static int counter = 0;
 
@@ -60,93 +75,43 @@ char *getRandomCourse(){
         if(!sample_file){printf("Erro no arquivo de amostras de cursos!"); exit(0);}
 
         sampleList = (char**) malloc (sizeof(char*));
-        char *aux = NULL;
-        while(fscanf(sample_file, "%s\n", aux)){
+        char *aux = (char*) malloc (sizeof(char)*16);
+
+        while(fscanf(sample_file, "%s\n", aux) == 1){
             sampleList[counter] = strdup(aux);
             counter++;
-            sampleList = (char**) realloc (sampleList, sizeof(char*)*counter);
+            sampleList = (char**) realloc (sampleList, sizeof(char*)*(counter+1));
         }
 
         fclose(sample_file);
     }
 
-    return sampleList[rand()%counter];
-}
-/*
-char **readNameList(int *counter){
-    FILE *namesfile = fopen("namelist.txt", "r+");
-    
-    char **namelist = (char**) malloc (sizeof(char*));
-    char *aux;
-
-    
-    fclose(namesfile);
-    return namelist;
-}
-
-char **readSurnameList(int *counter){
-    FILE *surnamefile = fopen("surnamelist.txt", "r+");
-
-    char **surnamelist = (char**) malloc (sizeof(char*));
-    char *aux;
-
-    while(fscanf(surnamefile, "%s\n", aux)){
-        surnamelist[*counter] = strdup(aux);
-        (*counter)++;
-        surnamelist = (char**) realloc (surnamelist, sizeof(char*)*(*counter));
+    if(flag)
+        return sampleList[rand()%counter];
+    else{
+        for(int i = 0; i < counter; i++)
+            free(sampleList[i]);
+        free(sampleList);
+        return NULL;
     }
-
-    fclose(surnamefile);
-    return surnamelist;
 }
 
-char **readCourseList(int *counter){
-    FILE *coursefile = fopen("courselist.txt", "r+");
-
-    char **courselist = (char**) malloc (sizeof(char*));
-    char *aux;
-
-    while(fscanf(coursefile, "%s\n", aux)){
-        courselist[*counter] = strdup(aux);
-        (*counter)++;
-        courselist = (char**) realloc (courselist, sizeof(char*)*(*counter));
-    }
-
-    fclose(coursefile);
-    return courselist;
-}*/
 
 int main(){
 
-    srand(time(NULL));
-
     FILE *data_file = fopen("entries.txt", "w+");
-
     int quantity, i;
-    /*char **namelist;
-    char **surnamelist;
-    char **courselist;
-    
-    int namecount = 0, surnamecount = 0, coursecount = 0;*/
-    char *auxname;
-    char *auxlastname;
-    char *auxcourse;
+
+    srand(time(NULL));
     scanf("%d", &quantity);
 
-   // namelist = readNameList(&namecount);
-   // surnamelist = readSurnameList(&surnamecount);
-   // courselist = readCourseList(&coursecount);
-
-    for(i = 0; i < quantity; i++){
-        auxname = getRandomName();
-        printf("%s\n", auxname);
-        auxlastname = getRandomLastName();
-        auxcourse = getRandomCourse();
-        //fprintf(data_file, "1\n%d\n%s\n%s\n%s\n%f\n", i, namelist[rand()%namecount], surnamelist[rand()%surnamecount], courselist[rand()%coursecount], (float)(rand()%11));
-        fprintf(data_file, "1\n%d\n%s%s%s%f\n", i, auxname, auxlastname, auxcourse, (float)(rand()%11));
-    }
+    for(i = 0; i < quantity; i++)
+        fprintf(data_file, "1\n%d\n%s\n%s\n%s\n%f\n", (i+200000), getRandomName(1), getRandomLastName(1), getRandomCourse(1), (float)(rand()%11));
 
     fclose(data_file);
+    getRandomName(0);
+    getRandomLastName(0);
+    getRandomCourse(0);
 
     return 0;
 }
